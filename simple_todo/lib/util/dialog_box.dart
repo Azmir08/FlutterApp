5503b04174aba9ac0ef1fc20272b4d477d62f852
Azmir <azmirmahni08@gmail.com>
2025-05-14 00:07:52 +0800

    todoApp

---
+++ simple_todo/lib/util/dialog_box.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:simple_todo/util/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  const DialogBox({
+    super.key,
+    required this.controllers,
+    required this.onSave,
+    required this.onCancel,
+  });
+  final TextEditingController controllers;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: controllers,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                MyButton(text: "Save", onPressed: onSave),
+                const SizedBox(width: 8),
+                MyButton(text: "Cancel", onPressed: onCancel),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
