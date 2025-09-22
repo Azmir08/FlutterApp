d9b91b31ec149e6731b87322751877db4185ad0d
Azmir <azmirmahni08@gmail.com>
2025-09-22 23:14:21 +0800

    second commit

---
+++ blog_app/lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://zkoyfsdigxwgjbgjevjo.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inprb3lmc2RpZ3h3Z2piZ2pldmpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NDc4OTgsImV4cCI6MjA3NDEyMzg5OH0.0n0mvTf3fWFXk0AxxMGIfjld3NBwdYdaPv3vnIEU8aU';
+}
